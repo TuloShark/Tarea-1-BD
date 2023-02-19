254c124d684663c6efb168b3fdc65c738c556aaa
Wander Jimenez Calvo <86485839+TuloShark@users.noreply.github.com>
2023-02-19 08:53:19 -0600

    Add files via upload
    Se creo la base de datos y su primera tabla.
    Se introdujeron los 10 valores solicitados en los datos de tabla.

---
+++ dbo.Articulo.data.sql
@@ -0,0 +1,12 @@
+﻿INSERT INTO [dbo].[Articulo] ([Nombre], [Precio]) 
+VALUES 
+('Martillo', 2000.0000),
+('Sierra', 5000.0000),
+('Desatornillador', 3000.0000),
+('Navaja', 1500.0000),
+('Tubo PVC', 1000.0000),
+('Tornillo', 300.0000),
+('Cincel', 4000.0000),
+('Pala', 5000.0000),
+('Llave', 3500.0000),
+('Clavo', 200.0000);
